c54156ac24c9b8fb35cb16436517ef7c7f45ba60
moonFaker <45261398+moonFaker@users.noreply.github.com>
2019-12-04 19:54:26 +0800

    Update UserController.java
    16

--- userservice/src/main/java/com/myproject/uservice/constroller/UserController.java
+++ userservice/src/main/java/com/myproject/uservice/constroller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @HystrixCommand(fallbackMethod = "fallBack")
     public  Object findById(@RequestParam("userId")String userId,@RequestParam("testId")String testId){
 
-        System.out.println("进入13");
+        System.out.println("进入16");
         return    restTemplate.getForObject("http://mybatis/api/v1/findById="+userId,Object.class);
         //return  userClient.findByIds(userId);
     }
