6f105c0b7f8e501d6437a5a4ebf2782bc4122ef1
mrs <mrs@163.com>
2019-12-04 19:55:38 +0800

    18

--- userservice/src/main/java/com/myproject/uservice/constroller/UserController.java
+++ userservice/src/main/java/com/myproject/uservice/constroller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @HystrixCommand(fallbackMethod = "fallBack")
     public  Object findById(@RequestParam("userId")String userId,@RequestParam("testId")String testId){
 
-        System.out.println("进入10");
+        System.out.println("进入15");
         return    restTemplate.getForObject("http://mybatis/api/v1/findById="+userId,Object.class);
         //return  userClient.findByIds(userId);
     }
